c1c0847d09c6d116f2615def4ecebe0409db11e8
Joel Helbling <joel@joelhelbling.com>
2018-04-30 11:51:28 -0400

    Generated gem

---
+++ lib/immutable/hash.rb
@@ -0,0 +1,7 @@
+require "immutable/hash/version"
+
+module Immutable
+  module Hash
+    # Your code goes here...
+  end
+end
---
+++ lib/immutable/hash/version.rb
@@ -0,0 +1,5 @@
+module Immutable
+  module Hash
+    VERSION = "0.1.0"
+  end
+end
